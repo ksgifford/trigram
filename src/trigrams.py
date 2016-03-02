d30eb63d4633654e24647bd979725b837cbf76a4
Norton Pengra <npengra317@gmail.com>
2016-03-02 13:51:47 -0800

    Created function frameworks

--- src/trigrams.py
+++ src/trigrams.py
@@ -1,6 +1,21 @@
+import io
+
 
 def main(source_file, generation_amt):
     pass
+
+
+def read_file(source_file):
+    pass
+
+
+def parse_text(raw_text):
+    pass
+
+
+def create_trigram(text):
+    for i, word in enumerate(text.split()):
+        trigram[(text[i:i + 1], text[i + 1:i + 2])] = text[i + 2:i + 3]
 
 
 if __name__ == '__main__':
